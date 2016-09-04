718b267546b2a1412fd7ee830f31c464ee0b1f5f
lizuyao2010 <zuyaoli2010@gmail.com>
2016-09-04 00:04:33 -0700

    update load vgg features

--- dataUtils.py
+++ dataUtils.py
@@ -79,7 +79,7 @@ def resize_image(img):
 def resize_image_from_file(file):
     img=misc.imread(file)
     print "origin shape",img.shape
-    img=resize_image(img)[:,:,3]
+    img=resize_image(img)[:,:,:3]
     print "resized shape",img.shape
     return img
 
